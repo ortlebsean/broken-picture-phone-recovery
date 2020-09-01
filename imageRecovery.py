144e170048114187fffd8c5971aa66873506f2f1
ortlebsean <49533359+ortlebsean@users.noreply.github.com>
2020-09-01 12:54:45 -0600

    Update imageRecovery.py

--- imageRecovery.py
+++ imageRecovery.py
@@ -3,21 +3,28 @@ from bs4 import BeautifulSoup
 import base64
 import io
 
+
+def bookHandle(book):
+	bookName = book.find("h2").text.split("'")[0]
+	pages = book.findAll("section")
+	pageHandle(bookName,pages)
+
+def pageHandle(bookName,pages):
+	for page in pages:
+		subInfo = page.find("h3").text.replace(",","").replace(":","").replace(" ","-")
+		fileName="BPP-"+bookName+"-"+subInfo+".png"
+		image=page.find("img")
+		imst = str(image).replace(' ','+').replace('<img+src="data:image/png;base64,','').replace('"/>','')
+		dcode = base64.decodebytes(bytes(imst,'utf-8'))
+		fh=open(fileName,'wb')
+		fh.write(dcode)
+		fh.close()
+
+
+
 bpp1 = open("bpp1.html", "r", encoding="utf-8")
 bpp = BeautifulSoup(bpp1, "lxml")
-pic = bpp.findAll('img')
+books = bpp.findAll("article")
+for book in books:
+	bookHandle(book)
 
-#print(pic[0])
-
-i = 0
-for image in pic:
-	imst = str(image).replace(' ','+').replace('<img+src="data:image/png;base64,','').replace('"/>','')
-	dcode = base64.decodebytes(bytes(imst,'utf-8'))
-	#print(imst)
-	filen = 'bppimg'+str(i)+'.png'
-	i = i + 1
-	print(filen)
-	#print(dcode)
-	fh=open(filen,'wb')
-	fh.write(dcode)
-	fh.close()
